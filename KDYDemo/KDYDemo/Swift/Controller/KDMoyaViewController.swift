0b9e91a1978e5f8f59d2953c345fdfd984ea14c6
kaideyi <kaideyi@haodai.com>
2016-09-05 18:37:07 +0800

    update Moya

---
+++ KDYDemo/KDYDemo/Swift/Controller/KDMoyaViewController.swift
@@ -0,0 +1,20 @@
+//
+//  KDMoyaViewController.swift
+//  KDYDemo
+//
+//  Created by mac on 16/9/5.
+//  Copyright © 2016年 kaideyi.com. All rights reserved.
+//
+
+import UIKit
+import Moya
+
+/// Moya与RxSwift并且利用SwiftJSON解析
+class KDMoyaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
