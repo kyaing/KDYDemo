874a20c407d682c1751a8c6848634d3c11373d5e
kaideyi <kaideyi@zoneyet.com>
2016-09-17 19:38:41 +0800

    KDYWeChat 初步集成 环信

--- KDYWeChat/KDYWeChat/AppDelegate.swift
+++ KDYWeChat/KDYWeChat/AppDelegate.swift
@@ -8,20 +8,45 @@
 
 import UIKit
 
+// 环信配置参数
+let emAppKey         = "kdy#kdywechat"
+let emClinetId       = "YXA6Y7mEEHfkEeaj5e2MKO4LDw"
+let emClientSecret   = "YXA6s_ZXqJD7UKFkvubKfv1_9BXDIKI"
+let emApnsDevCerName = "kdychat_devleop"
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     var tabbarController: KDTabBarController?
 
+    // MARK: - AppDelegate
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
         // 设置根视图
         setupRootController()
         
+        // 配置环信
+        // 注册的 AppKey，和推送证书名
+        let options = EMOptions(appkey: emAppKey)
+        options.apnsCertName = emApnsDevCerName
+        
+        EMClient.sharedClient().initializeSDKWithOptions(options)
+        
         return true
     }
     
+    // App进入后台
+    func applicationDidEnterBackground(application: UIApplication) {
+        EMClient.sharedClient().applicationDidEnterBackground(application)
+    }
+    
+    // App将要进入前台
+    func applicationWillEnterForeground(application: UIApplication) {
+        EMClient.sharedClient().applicationWillEnterForeground(application)
+    }
+    
+    // MARK: - Private Methods
     func setupRootController() {
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
         window?.backgroundColor = UIColor.whiteColor()
--- KDYWeChat/KDYWeChat/Vendor/HyphenateFullSDK/include/EMSDK.h
+++ KDYWeChat/KDYWeChat/Vendor/HyphenateFullSDK/include/EMSDK.h
@@ -15,17 +15,17 @@
 #ifndef EMSDK_h
 #define EMSDK_h
 
-#if TARGET_OS_IPHONE
+//#if TARGET_OS_IPHONE
 
 #import "EMClient.h"
 #import "EMClientDelegate.h"
 
-#else
+//#else
 
-#import <HyphenateSDK/EMClient.h>
-#import <HyphenateSDK/EMClientDelegate.h>
+//#import <HyphenateSDK/EMClient.h>
+//#import <HyphenateSDK/EMClientDelegate.h>
 
-#endif
-
+//#endif
 
 #endif /* EMSDK_h */
+
