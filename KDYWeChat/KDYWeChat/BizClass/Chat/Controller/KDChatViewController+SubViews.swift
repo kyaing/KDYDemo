ea1b6569ba2f05e7324d33c48a4977230fa70861
kaideyi <kaideyi@zoneyet.com>
2016-09-24 00:01:25 +0800

    聊天页面TableView能正常显示(为什么top要偏移64)

--- KDYWeChat/KDYWeChat/BizClass/Chat/Controller/KDChatViewController+SubViews.swift
+++ KDYWeChat/KDYWeChat/BizClass/Chat/Controller/KDChatViewController+SubViews.swift
@@ -11,91 +11,85 @@ import Foundation
 // MARK: - SubViews
 extension KDChatViewController {
     
-//    func setupChildViews() {
-//        setupBottomBarView()
-//        setupChatTableView()
-//        setupEmotionKeyboard()
-//        setupShareKeyboard()
-//    }
-//    
-//    /**
-//     * 初始化底部视图
-//     */
-//    func setupBottomBarView() {
-//        bottomBarView = NSBundle.mainBundle().loadNibNamed("ChatBottomBarView", owner: nil, options: nil).last as! ChatBottomBarView
-//        bottomBarView.delegate = self
-//        bottomBarView.inputTextView.delegate = self
-//        view.addSubview(bottomBarView)
-//        
-//        bottomBarView.snp_makeConstraints { (make) in
-//            make.left.right.equalTo(self.view)
-//            make.height.equalTo(kBarViewHeight)
-//            
-//            // BarView底部的约束
-//            barPaddingBottomConstranit = make.bottom.equalTo(view.snp_bottom).constraint
-//        }
-//    }
-//    
-//    func setupChatTableView() {
-//        view.backgroundColor = UIColor.whiteColor()
-//        
-//        chatTableView = UITableView(frame: CGRect.zero, style: .Plain)
-//        chatTableView.backgroundColor = UIColor.clearColor()
-//        chatTableView.tableFooterView = UIView()
-//        chatTableView.separatorStyle = .SingleLine
-//        chatTableView.dataSource = self
-//        chatTableView.delegate = self
-//        
-//        view.addSubview(self.chatTableView)
-//        
-//        chatTableView.snp_makeConstraints { (make) in
-//            make.top.left.right.equalTo(self.view)
-//            make.bottom.equalTo(self.bottomBarView.snp_top)
-//        }
-//        
-//        // 添加点击手势，隐藏所有键盘
-//        let tapGesture = UITapGestureRecognizer()
-//        tapGesture.cancelsTouchesInView = false
-//        chatTableView.addGestureRecognizer(tapGesture)
-//        tapGesture.rx_event.subscribeNext { _ in
-//            self.hideAllKeyboard()
-//            }.addDisposableTo(self.disposeBag)
-//        
-//        // 注册Cell
-//        chatTableView.registerNib(UINib.init(nibName: NSStringFromClass(ChatTextTableCell), bundle: nil), forCellReuseIdentifier: "ChatTextTableCell")
-//        chatTableView.registerNib(UINib.init(nibName: NSStringFromClass(ChatImageTableCell), bundle: nil), forCellReuseIdentifier: "ChatImageTableCell")
-//        chatTableView.registerNib(UINib.init(nibName: NSStringFromClass(ChatAudioTableCell), bundle: nil), forCellReuseIdentifier: "ChatAudioTableCell")
-//        chatTableView.registerNib(UINib.init(nibName: NSStringFromClass(ChatLocationTableCell), bundle: nil), forCellReuseIdentifier: "ChatLocationTableCell")
-//        chatTableView.registerNib(UINib.init(nibName: NSStringFromClass(ChatRedEnvelopeCell), bundle: nil), forCellReuseIdentifier: "ChatRedEnvelopeCell")
-//    }
-//    
-//    /**
-//     * 初始化表情键盘
-//     */
-//    func setupEmotionKeyboard() {
-//        emotionView = NSBundle.mainBundle().loadNibNamed("ChatEmotionView", owner: nil, options: nil).last as! ChatEmotionView
-//        emotionView.backgroundColor = UIColor.yellowColor()
-//        view.addSubview(emotionView)
-//        
-//        emotionView.snp_makeConstraints { (make) in
-//            make.left.right.equalTo(view)
-//            make.height.equalTo(kInputKeyboardHeight)
-//        }
-//    }
-//    
-//    /**
-//     * 初始化扩展键盘
-//     */
-//    func setupShareKeyboard() {
-//        shareView = NSBundle.mainBundle().loadNibNamed("ChatShareMoreView", owner: nil, options: nil).last as! ChatShareMoreView
-//        shareView.backgroundColor = UIColor.blueColor()
-//        view.addSubview(shareView)
-//        
-//        shareView.snp_makeConstraints { (make) in
-//            make.left.right.equalTo(view)
-//            make.top.equalTo(bottomBarView.snp_bottom)
-//            make.height.equalTo(kInputKeyboardHeight)
-//        }
-//    }
+    func setupChildViews() {
+        setupBottomBarView()
+        setupChatTableView()
+        setupEmotionKeyboard()
+        setupShareKeyboard()
+    }
+    
+    /**
+     * 初始化底部视图
+     */
+    func setupBottomBarView() {
+        self.bottomBarView = NSBundle.mainBundle().loadNibNamed("ChatBottomBarView", owner: nil, options: nil).last as! ChatBottomBarView
+        self.bottomBarView.delegate = self
+        self.bottomBarView.inputTextView.delegate = self
+        view.addSubview(self.bottomBarView)
+        
+        self.bottomBarView.snp_makeConstraints { (make) in
+            make.left.right.equalTo(self.view)
+            make.height.equalTo(kBarViewHeight)
+            
+            // BarView底部的约束
+            barPaddingBottomConstranit = make.bottom.equalTo(view.snp_bottom).constraint
+        }
+    }
+    
+    func setupChatTableView() {
+        view.addSubview(self.chatTableView)
+        
+        self.chatTableView.snp_makeConstraints { (make) in
+            make.left.equalTo(self.view.snp_left)
+            make.right.equalTo(self.view.snp_right)
+            make.top.equalTo(self.view.snp_top).offset(64)  // 为什么top 要下偏移64?
+            make.bottom.equalTo(self.bottomBarView.snp_top)
+        }
+        
+        // 添加点击手势，隐藏所有键盘
+        let tapGesture = UITapGestureRecognizer()
+        tapGesture.cancelsTouchesInView = false
+        self.chatTableView.addGestureRecognizer(tapGesture)
+        tapGesture.rx_event.subscribeNext { _ in
+            self.hideAllKeyboard()
+            }.addDisposableTo(self.disposeBag)
+        
+        // 注册Cell
+        self.chatTableView.registerNib(UINib.init(nibName: NSStringFromClass(ChatTextTableCell), bundle: nil), forCellReuseIdentifier: "ChatTextTableCell")
+        self.chatTableView.registerNib(UINib.init(nibName: NSStringFromClass(ChatImageTableCell), bundle: nil), forCellReuseIdentifier: "ChatImageTableCell")
+        self.chatTableView.registerNib(UINib.init(nibName: NSStringFromClass(ChatAudioTableCell), bundle: nil), forCellReuseIdentifier: "ChatAudioTableCell")
+        self.chatTableView.registerNib(UINib.init(nibName: NSStringFromClass(ChatLocationTableCell), bundle: nil), forCellReuseIdentifier: "ChatLocationTableCell")
+        self.chatTableView.registerNib(UINib.init(nibName: NSStringFromClass(ChatRedEnvelopeCell), bundle: nil), forCellReuseIdentifier: "ChatRedEnvelopeCell")
+    }
+    
+    /**
+     * 初始化表情键盘
+     */
+    func setupEmotionKeyboard() {
+        self.emotionView = NSBundle.mainBundle().loadNibNamed("ChatEmotionView", owner: nil, options: nil).last as! ChatEmotionView
+        self.emotionView.backgroundColor = UIColor.yellowColor()
+        view.addSubview(self.emotionView)
+        
+        self.emotionView.snp_makeConstraints { (make) in
+            make.left.right.equalTo(view)
+            make.top.equalTo(self.bottomBarView.snp_bottom)
+            make.height.equalTo(kCustomKeyboardHeight)
+        }
+    }
+    
+    /**
+     * 初始化扩展键盘
+     */
+    func setupShareKeyboard() {
+        self.shareView = NSBundle.mainBundle().loadNibNamed("ChatShareMoreView", owner: nil, options: nil).last as! ChatShareMoreView
+        self.shareView.backgroundColor = UIColor.blueColor()
+        view.addSubview(self.shareView)
+        
+        self.shareView.snp_makeConstraints { (make) in
+            make.left.right.equalTo(view)
+            make.top.equalTo(self.bottomBarView.snp_bottom)
+            make.height.equalTo(kCustomKeyboardHeight)
+        }
+    }
 }
 
--- KDYWeChat/KDYWeChat/BizClass/Chat/Controller/KDChatViewController.swift
+++ KDYWeChat/KDYWeChat/BizClass/Chat/Controller/KDChatViewController.swift
@@ -16,7 +16,16 @@ let kCustomKeyboardHeight: CGFloat = 216
 /// 聊天界面 (学习TSWeChat)
 final class KDChatViewController: UIViewController, UITextViewDelegate {
     
-    var chatTableView: UITableView!
+    lazy var chatTableView: UITableView = {
+        let chatTableView = UITableView(frame: CGRect.zero, style: .Plain)
+        chatTableView.backgroundColor = UIColor.clearColor()
+        chatTableView.dataSource = self
+        chatTableView.delegate = self
+        chatTableView.separatorStyle = .SingleLine
+        
+        return chatTableView
+    }()
+
     var bottomBarView: ChatBottomBarView!
     var barPaddingBottomConstranit: Constraint?
     
@@ -28,6 +37,7 @@ final class KDChatViewController: UIViewController, UITextViewDelegate {
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.whiteColor()
         
         // 添加子视图
         setupChildViews()
@@ -37,94 +47,6 @@ final class KDChatViewController: UIViewController, UITextViewDelegate {
         
         // 键盘控制
         keyboardControl()
-    }
-    
-    func setupChildViews() {
-        setupBottomBarView()
-        setupChatTableView()
-        setupEmotionKeyboard()
-        setupShareKeyboard()
-    }
-    
-    /**
-     * 初始化底部视图
-     */
-    func setupBottomBarView() {
-        self.bottomBarView = NSBundle.mainBundle().loadNibNamed("ChatBottomBarView", owner: nil, options: nil).last as! ChatBottomBarView
-        self.bottomBarView.delegate = self
-        self.bottomBarView.inputTextView.delegate = self
-        view.addSubview(self.bottomBarView)
-        
-        self.bottomBarView.snp_makeConstraints { (make) in
-            make.left.right.equalTo(self.view)
-            make.height.equalTo(kBarViewHeight)
-            
-            // BarView底部的约束
-            barPaddingBottomConstranit = make.bottom.equalTo(view.snp_bottom).constraint
-        }
-    }
-    
-    func setupChatTableView() {
-        view.backgroundColor = UIColor.whiteColor()
-        
-        self.chatTableView = UITableView(frame: CGRect.zero, style: .Plain)
-        self.chatTableView.backgroundColor = UIColor.clearColor()
-        self.chatTableView.tableFooterView = UIView()
-        self.chatTableView.separatorStyle = .SingleLine
-        self.chatTableView.dataSource = self
-        self.chatTableView.delegate = self
-    
-        view.addSubview(self.chatTableView)
-        
-        self.chatTableView.snp_makeConstraints { (make) in
-            make.top.left.right.equalTo(self.view)
-            make.bottom.equalTo(self.bottomBarView.snp_top)
-        }
-        
-        // 添加点击手势，隐藏所有键盘
-        let tapGesture = UITapGestureRecognizer()
-        tapGesture.cancelsTouchesInView = false
-        self.chatTableView.addGestureRecognizer(tapGesture)
-        tapGesture.rx_event.subscribeNext { _ in
-            self.hideAllKeyboard()
-            }.addDisposableTo(self.disposeBag)
-        
-        // 注册Cell
-        self.chatTableView.registerNib(UINib.init(nibName: NSStringFromClass(ChatTextTableCell), bundle: nil), forCellReuseIdentifier: "ChatTextTableCell")
-        self.chatTableView.registerNib(UINib.init(nibName: NSStringFromClass(ChatImageTableCell), bundle: nil), forCellReuseIdentifier: "ChatImageTableCell")
-        self.chatTableView.registerNib(UINib.init(nibName: NSStringFromClass(ChatAudioTableCell), bundle: nil), forCellReuseIdentifier: "ChatAudioTableCell")
-        self.chatTableView.registerNib(UINib.init(nibName: NSStringFromClass(ChatLocationTableCell), bundle: nil), forCellReuseIdentifier: "ChatLocationTableCell")
-        self.chatTableView.registerNib(UINib.init(nibName: NSStringFromClass(ChatRedEnvelopeCell), bundle: nil), forCellReuseIdentifier: "ChatRedEnvelopeCell")
-    }
-    
-    /**
-     * 初始化表情键盘
-     */
-    func setupEmotionKeyboard() {
-        self.emotionView = NSBundle.mainBundle().loadNibNamed("ChatEmotionView", owner: nil, options: nil).last as! ChatEmotionView
-        self.emotionView.backgroundColor = UIColor.yellowColor()
-        view.addSubview(self.emotionView)
-        
-        self.emotionView.snp_makeConstraints { (make) in
-            make.left.right.equalTo(view)
-            make.top.equalTo(self.bottomBarView.snp_bottom)
-            make.height.equalTo(kCustomKeyboardHeight)
-        }
-    }
-    
-    /**
-     * 初始化扩展键盘
-     */
-    func setupShareKeyboard() {
-        self.shareView = NSBundle.mainBundle().loadNibNamed("ChatShareMoreView", owner: nil, options: nil).last as! ChatShareMoreView
-        self.shareView.backgroundColor = UIColor.blueColor()
-        view.addSubview(self.shareView)
-        
-        self.shareView.snp_makeConstraints { (make) in
-            make.left.right.equalTo(view)
-            make.top.equalTo(self.bottomBarView.snp_bottom)
-            make.height.equalTo(kCustomKeyboardHeight)
-        }
     }
 }
 
